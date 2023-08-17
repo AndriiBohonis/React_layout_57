34983a5296c943259d27e3f2dd6163e060c6385e
Andrii <andriy30534>
2023-08-17 21:32:48 +0300

    fix wrapper

--- src/hoc/Wrapper/Wrapper.tsx
+++ src/hoc/Wrapper/Wrapper.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
-import s from './Wrapper.module.scss'
+import s from './Wrapper.module.css'
 export const Wrapper = ({ children }: { children: ReactNode }) => {
 	return (
 		<div className={s.wrapper}>
-			<div className={s.content_wrapper}>{children}</div>
+			<div className={s.content}>{children}</div>
 		</div>
 	)
 }
